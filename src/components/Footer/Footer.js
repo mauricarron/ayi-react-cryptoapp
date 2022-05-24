1a86d13796f1b895c2f0d03f9204c9b7b3687766
mdcarron <mdcarron.dev@gmail.com>
2022-05-24 15:44:03 -0300

    Add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,36 @@
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
+import { Link, Paper } from "@mui/material";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import CurrencyBitcoinIcon from "@mui/icons-material/CurrencyBitcoin";
 
 const Footer = () => {
   return (
-    <footer style={{ color: "gray", position: "fixed", bottom: 0 }}>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="fixed">
-          <Toolbar>
-            <Typography>Copyright</Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
+    <footer
+      style={{
+        color: "gray",
+        width: "100%",
+        height: "4rem",
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+      }}
+    >
+      <Paper elevation={10}>
+        <Toolbar sx={{ justifyContent: "flex-end" }}>
+          <Typography>
+            Hecho con mucho 💚 para <b>AYI Academy</b>{" "}
+            <Link
+              href="https://www.linkedin.com/in/uricarron/"
+              target="_blank"
+              rel="noopener"
+              color="inherit"
+              underline="none"
+            >
+              @Carron Mauricio David
+            </Link>{" "}
+            | Mayo 2022
+          </Typography>
+        </Toolbar>
+      </Paper>
     </footer>
   );
 };
