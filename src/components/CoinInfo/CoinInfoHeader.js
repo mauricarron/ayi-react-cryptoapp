9f6f112568395ea7b98dbaa25bdd926e01bf0c15
mdcarron <mdcarron.dev@gmail.com>
2022-05-25 21:25:18 -0300

    Add go back button

--- src/components/CoinInfo/CoinInfoHeader.js
+++ src/components/CoinInfo/CoinInfoHeader.js
@@ -1,21 +1,35 @@
 import PropTypes from "prop-types";
-import { Avatar, Typography, Grid } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { Avatar, Typography, Grid, Button } from "@mui/material";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 
 const CoinInfoHeader = ({ id, icon, name, symbol }) => {
+  const navigate = useNavigate();
   return (
-    <Grid container spacing={2} direction="row" alignItems="center" mb={2}>
-      <Grid item>
-        <Avatar alt={id} src={icon} sx={{ width: 64, height: 64 }} />
+    <>
+      <Grid container direction="row" mb={2}>
+        <Button
+          variant="text"
+          startIcon={<ArrowBackIosNewIcon />}
+          onClick={() => navigate(-1)}
+        >
+          Back to list
+        </Button>
       </Grid>
-      <Grid container item direction="row" alignItems="baseline" xs={11}>
-        <Typography variant="h2" component="h1" mr={1}>
-          {name}
-        </Typography>
-        <Typography variant="h6" color="gray">
-          {symbol}
-        </Typography>
+      <Grid container spacing={2} direction="row" alignItems="center" mb={2}>
+        <Grid item>
+          <Avatar alt={id} src={icon} sx={{ width: 64, height: 64 }} />
+        </Grid>
+        <Grid container item direction="row" alignItems="baseline" xs={11}>
+          <Typography variant="h2" component="h1" mr={1}>
+            {name}
+          </Typography>
+          <Typography variant="h6" color="gray">
+            {symbol}
+          </Typography>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
 
