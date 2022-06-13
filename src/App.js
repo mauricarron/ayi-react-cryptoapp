b70f9167269d5883e36a4602a6a5be8e458a2728
mdcarron <mdcarron.dev@gmail.com>
2022-06-13 19:02:06 -0300

    Update app routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import AboutView from "./views/About/AboutView";
-import CoinDetailView from "./views/CoinDetail/CoinDetailView";
-import CoinsListView from "./views/CoinsList/CoinsListView";
-import HomeView from "./views/Home/HomeView";
-import NotFoundView from "./views/NotFound/NotFoundView";
+import AppRoutes from "./components/AppRoutes/AppRoutes";
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route index element={<HomeView />} />
-          <Route path="/coins" element={<CoinsListView />} />
-          <Route path="/coins/:id" element={<CoinDetailView />} />
-          <Route path="/about" element={<AboutView />} />
-          <Route path="*" element={<NotFoundView />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
---
+++ src/components/AppRoutes/AppRoutes.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import AboutView from "../../views/About/AboutView";
+import CoinDetailView from "../../views/CoinDetail/CoinDetailView";
+import CoinsListView from "../../views/CoinsList/CoinsListView";
+import HomeView from "../../views/Home/HomeView";
+import NotFoundView from "../../views/NotFound/NotFoundView";
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route index element={<HomeView />} />
+          <Route path="/coins" element={<CoinsListView />} />
+          <Route path="/coins/:id" element={<CoinDetailView />} />
+          <Route path="/about" element={<AboutView />} />
+          <Route path="*" element={<NotFoundView />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default AppRoutes;
